f5d4b30b014dcd74f96b17017e7fe8c33c749b92
Diwash <diwashb999@gmail.com>
2023-04-05 09:00:31 +0545

    Miscellaneous Section

---
+++ day-8/pages/about.js
@@ -0,0 +1,25 @@
+import Footer from "@/components/Footer";
+import Head from "next/head";
+
+function About() {
+  return (
+    <>
+      <Head>
+        {/* <title>About Codevolution</title> */}
+        <meta name="description" content="Free Tutorials on web development" />
+      </Head>
+      <h1 className="content">About</h1>
+    </>
+  );
+}
+
+export default About;
+
+About.getLayout = function PageLayout(page) {
+  return (
+    <>
+      {page}
+      <Footer />
+    </>
+  );
+};
