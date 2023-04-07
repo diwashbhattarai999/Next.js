197142637765c5b4d0a9c3566186f858540e354a
Diwash <diwashb999@gmail.com>
2023-04-07 19:51:09 +0545

    Misc: redirects & env

--- day-8/pages/[anime].js
+++ day-8/pages/[anime].js
@@ -4,14 +4,14 @@ import img from '../public/1.jpg'
 function AnimePage() {
   return (
     <div className="img-container">
-        {/* <Image src={img} alt="anime" placeholder="blur" blurDataURL=" " width="320" height="220" />
+        <Image src={img} alt="anime" placeholder="blur" blurDataURL=" " width="320" height="220" />
       {["1", "2", "3", "4", "5"].map((path) => {
         return (
           <div key={path}>
             <Image src={`/${path}.jpg`} alt="anime" width="320" height="220" />
           </div>
         );
-      })} */}
+      })}
     </div>
   );
 }
