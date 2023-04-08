dc065fe6485aa49fb4d15f6564f6f49725a0f592
Diwash <diwashb999@gmail.com>
2023-04-08 16:03:49 +0545

    UI for login form

--- day-9/layout/layout.js
+++ day-9/layout/layout.js
@@ -1,10 +1,18 @@
-function Layout({ children }) {
+import styles from "@/styles/Layout.module.css";
+
+export default function Layout({ children }) {
   return (
-    <div className="flex h-screen bg-blue-200 ">
-      <h1>Layout</h1>
-      {children}
+    <div className="flex min-h-screen py-2 bg-blue-200">
+      <div className="m-auto bg-slate-50 rounded-md w-3/5  grid lg:grid-cols-2">
+        <div className={styles.imgStyle}>
+          <div className={styles.cartoonImg}></div>
+          <div className={styles.cloud_one}></div>
+          <div className={styles.cloud_two}></div>
+        </div>
+        <div className="right flex flex-col justify-evenly h-full">
+          <div className="text-center py-10">{children}</div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default Layout;
--- day-9/pages/index.js
+++ day-9/pages/index.js
@@ -1,19 +1,70 @@
 import { Poppins } from "next/font/google";
 import Head from "next/head";
+import Link from "next/link";
+import { useState } from "react";
 
 const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 
 export default function Home() {
+  const [session, setSession] = useState(false);
+
   return (
     <>
       <Head>
         <title>Home Page</title>
       </Head>
-      <main
-        className={`${poppins.className} flex min-h-screen flex-col items-center justify-between p-24`}
-      >
-        <h1 className=" text-3xl font-bold underline">Hello world!</h1>
-      </main>
+      {session ? User() : Guest()}
     </>
+  );
+}
+
+//Guest
+function Guest() {
+  return (
+    <main
+      className={`${poppins.className} flex flex-col items-center justify-center h-screen container mx-auto text-center`}
+    >
+      <h3 className="text-4xl font-bold">Guest Homepage</h3>
+
+      <div className="flex justify-center">
+        <Link
+          href={"/login"}
+          className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50"
+        >
+          Sign In
+        </Link>
+      </div>
+    </main>
+  );
+}
+
+//Authorized User
+function User() {
+  return (
+    <main
+      className={`${poppins.className} flex flex-col items-center justify-center h-screen container mx-auto text-center`}
+    >
+      <h3 className="text-4xl font-bold">Authorized User Homepage</h3>
+
+      <div className="details">
+        <h5>Unknown</h5>
+        <h5>Unknown</h5>
+      </div>
+
+      <div className="flex justify-center">
+        <button className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 bg-gray-50">
+          Sign Out
+        </button>
+      </div>
+
+      <div className="flex justify-center">
+        <Link
+          href={"/profile"}
+          className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50"
+        >
+          Profile Page
+        </Link>
+      </div>
+    </main>
   );
 }
